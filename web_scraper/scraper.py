ffaa15ffcbf75c23f7aaae4523c090fc78b9364c
corey-marchand <corey.marchand@me.com>
2020-06-07 20:38:46 -0700

    citation count function complte

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -9,26 +9,27 @@ response = requests.get(URL)
 # Get the content 
 content = response.content
 # print(content)
+
 # Converting to Beautiful soup object
 soup = BeautifulSoup(content, 'html.parser')
 # print(soup.prettify())
+
 # Find an element 
-results = soup.findAll('a href', title = "Category")
-print(results)
+results = soup.findAll('sup', class_="noprint Inline-Template Template-Fact")
+# print(results)
 
 def get_citations_needed_count(URL):
-    citation = results
-    return len(citation)
+    return len(results)
 
-def get_citations_list(URL):
-    citation = results 
-    citation_list = []
+# def get_citations_list(URL):
+    # citation = results 
+    # citation_list = []
 
-    for cit in citation:
-        cit = cit.parent.text.strip()
-        citation_list.append(cit)
-    return citation_list
+    # for cit in citation:
+    #     cit = cit.parent.text.strip()
+    #     citation_list.append(cit)
+    # return citation_list
 
 
-print(get_citations_list(URL))
 print(get_citations_needed_count(URL))
+# print(get_citations_needed_count(URL))
